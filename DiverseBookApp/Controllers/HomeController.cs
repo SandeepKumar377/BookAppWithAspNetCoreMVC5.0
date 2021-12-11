99aa09ef78c3ed2a3d6e4a2f8002ad35f8303ff9
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-12-11 21:38:47 +0530

    add model repo and ctrl

--- DiverseBookApp/Controllers/HomeController.cs
+++ DiverseBookApp/Controllers/HomeController.cs
@@ -8,9 +8,13 @@ namespace DiverseBookApp.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello Index";
+            return View();
+        }
+        public ViewResult AboutUs()
+        {
+            return View();
         }
     }
 }
---
+++ DiverseBookApp/Repository/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using DiverseBookApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiverseBookApp.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string bookName, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(bookName) || x.Author.Contains(authorName)).ToList();
+        }
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){ Id=1, Title="MVC", Author="Sandeep"},
+                new BookModel(){ Id=2, Title="JAVA", Author="Kumar"},
+                new BookModel(){ Id=3, Title="html", Author="Sendy"},
+                new BookModel(){ Id=4, Title="php", Author="Sandeep4"},
+                new BookModel(){ Id=5, Title="python", Author="Sandeep5"},
+            };
+        }
+    }
+}
+ 

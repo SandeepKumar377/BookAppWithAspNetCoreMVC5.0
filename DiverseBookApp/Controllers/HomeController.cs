b59d5dc95b6cc1737e27fc1fa51a9ec8f3f81f4c
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-12-12 19:41:37 +0530

    ViewData attribute

--- DiverseBookApp/Controllers/HomeController.cs
+++ DiverseBookApp/Controllers/HomeController.cs
@@ -8,16 +8,26 @@ namespace DiverseBookApp.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string CustomProperty { get; set; }
+        [ViewData]
+        public string Title { get; set; }
+        [ViewData]
+        public string Book { get; set; }
         public ViewResult Index()
         {
+            Title = "Home from Ctrl";
+            CustomProperty = "Sandeep kumar";
             return View();
         }
         public ViewResult AboutUs()
         {
+            Title = "About us from Ctrl";
             return View();
         }
         public ViewResult ContactUs()
         {
+            Title = "Contact us from Ctrl";
             return View();
         }
     }

5cfc9b33f1dca7e11ad572499b67a88a74e76640
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-12-15 01:12:30 +0530

    setup DbContext class

---
+++ DiverseBookApp/Data/BookAppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiverseBookApp.Data
+{
+    public class BookAppContext : DbContext
+    {
+        public BookAppContext(DbContextOptions<BookAppContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Books> Books { get; set; }
+    }
+}

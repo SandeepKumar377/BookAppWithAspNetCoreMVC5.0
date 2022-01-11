9af9f844638d863295d05f251f879828a821a19e
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2022-01-11 16:28:31 +0530

    Add Singup Page ande model and make separate page of common css

---
+++ DiverseBookApp/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using DiverseBookApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiverseBookApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("signup")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("signup")]
+        public IActionResult Signup(SignupUserModel signupUserModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+
+            }
+            return View();
+        }
+    }
+}
---
+++ DiverseBookApp/Models/SignupUserModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiverseBookApp.Models
+{
+    public class SignupUserModel
+    {
+        [Required(ErrorMessage ="Please enter email!")]
+        [EmailAddress(ErrorMessage ="Please enter valid Email!")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage ="Please enter Password!")]
+        [Compare("ConfirmPassword", ErrorMessage ="Password does not match!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        
+        [Required(ErrorMessage ="Please enter Confirm Password!")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }        
+    }
+}
